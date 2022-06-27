cd7aff53a79233be16a6551aa090e8fcce766af6
saffyalam <62318293+saffyalam@users.noreply.github.com>
2022-06-27 00:04:22 -0400

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,37 @@
+ # def cube(num):
+ #    return pow(num, 3)
+
+
+#----------Take input from user for an array -----------#
+
+from array import *
+
+arr = array("i",[])
+n = int(input("Enter the length of array: "))
+
+for i in range(n):   #(01234.....)
+    x = int(input("Enter array element: "))
+    arr.append(x)
+
+print(arr)
+
+# val = int(input("Enter the value to search: "))
+#
+# k = 0
+# for e in arr: #(9,0,8,7....)
+#     if e == val:
+#
+#        print(k)
+#        break
+#
+#     k = k+1
+# # print(arr.index(val)) #using direct function
+#
+#  k = 0
+#  for e in arr:
+#      if e == val:
+#          print("Value present in array")
+#     else:
+#         print("Value not in array")
+#
+#     k = k+1
